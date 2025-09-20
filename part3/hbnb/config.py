73c94660e163e16eb23bd11d2131addfe526554a
Abdullah-aldafas <138772608+Abdullah-aldafas@users.noreply.github.com>
2025-09-20 19:20:08 +0300

    Update config.py

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -2,10 +2,13 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key') 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost:5432/testdatabase') 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
